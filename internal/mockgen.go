06571881dee0066148ab4c6855ac3f7063f4aea0
Alles Alexey <aalles@torbor.ru>
2021-10-26 13:23:02 +0300

    fix: typo in readme

--- internal/mockgen.go
+++ internal/mockgen.go
@@ -2,4 +2,3 @@ package internal
 
 //go:generate mockgen -destination=./mocks/repo_mock.go -package=mocks github.com/ozonmp/bss-office-api/internal/app/repo EventRepo
 //go:generate mockgen -destination=./mocks/sender_mock.go -package=mocks github.com/ozonmp/bss-office-api/internal/app/sender EventSender
-//go:generate mockgen -destination=./mocks/test_mock.go -package=mocks github.com/ozonmp/bss-office-api/internal/app/test Foo
