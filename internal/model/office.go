592c8f7fb09b12b3592b8c2b66f16a8a5a3b2ea1
alexey alles <aalles@torbor.ru>
2021-11-03 22:33:23 +0300

    fix: del others

--- internal/model/office.go
+++ internal/model/office.go
@@ -1,11 +1,8 @@
 package model
 
-import "time"
-
 // Office model for office
 type Office struct {
-	ID          uint64    `db:"id"`
-	Name        string    `db:"name"`
-	Description string    `db:"description"`
-	Created     time.Time `db:"created"`
+	ID          uint64 `db:"id"`
+	Name        string `db:"name"`
+	Description string `db:"description"`
 }
