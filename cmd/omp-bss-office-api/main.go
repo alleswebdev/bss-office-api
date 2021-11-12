198995398bab8c399dc300ae035b8bbe554004ac
Alles Alexey <aalles@torbor.ru>
2021-11-12 14:17:30 +0300

    feat: add dummy sender and retranstator working main

--- cmd/omp-bss-office-api/main.go
+++ cmd/omp-bss-office-api/main.go
@@ -2,6 +2,18 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"github.com/ozonmp/bss-office-api/internal/app/sender"
+	"github.com/ozonmp/bss-office-api/internal/config"
+	"github.com/ozonmp/bss-office-api/internal/database"
+	"github.com/ozonmp/bss-office-api/internal/repo"
+
+	_ "github.com/jackc/pgx/v4"
+	_ "github.com/jackc/pgx/v4/stdlib"
+	_ "github.com/lib/pq"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 	"os"
 	"os/signal"
 	"syscall"
@@ -14,17 +26,51 @@ func main() {
 
 	sigs := make(chan os.Signal, 1)
 
-	cfg := retranslator.Config{
-		ChannelSize:    512,
-		ConsumerCount:  2,
-		ConsumeSize:    10,
-		ProducerCount:  28,
-		WorkerCount:    2,
-		ConsumeTimeout: time.Millisecond,
+	if err := config.ReadConfigYML("config.yml"); err != nil {
+		log.Fatal().Err(err).Msg("Failed init configuration")
 	}
 
-	retranslator := retranslator.NewRetranslator(cfg)
+	cfg := config.GetConfigInstance()
 
+	log.Info().
+		Str("version", cfg.Project.Version).
+		Str("commitHash", cfg.Project.CommitHash).
+		Bool("debug", cfg.Project.Debug).
+		Str("environment", cfg.Project.Environment).
+		Msgf("Starting service: %s", cfg.Project.Name)
+
+	if cfg.Project.Debug {
+		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	}
+
+	dsn := fmt.Sprintf("host=%v port=%v user=%v password=%v dbname=%v sslmode=%v",
+		cfg.Database.Host,
+		cfg.Database.Port,
+		cfg.Database.User,
+		cfg.Database.Password,
+		cfg.Database.Name,
+		cfg.Database.SslMode,
+	)
+
+	db, err := database.NewPostgres(dsn, cfg.Database.Driver)
+	if err != nil {
+		log.Fatal().Err(err).Msg("Failed init postgres")
+	}
+	defer db.Close()
+
+	rCfg := retranslator.Config{
+		ChannelSize:    512,
+		ConsumerCount:  10,
+		ConsumeSize:    10,
+		ConsumeTimeout: time.Millisecond,
+		ProducerCount:  28,
+		WorkerCount:    10,
+		Repo:           repo.NewEventRepo(db),
+		Sender:         sender.NewDummySender(),
+	}
+
+	retranslator := retranslator.NewRetranslator(rCfg)
+	defer retranslator.Close()
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
